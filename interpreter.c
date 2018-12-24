47d2af27fae90d3e4ba9d0dd9911566efe691ec8
Krzysztof Slawik <307020@uwr.edu.pl>
2018-12-24 23:31:54 +0100

    added some comments

--- interpreter.c
+++ interpreter.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#define MAXLENGTH 20
+#define MAXLENGTH 20//max length of any string representing tag, function name or variable
 
 typedef struct line
 {
-	char *first;
-	char *second;
-	char *third;
+	char *first;//string with tag
+	char *second;//string with name of function
+	char *third;//string with argument of function
 }line;
 
 typedef struct var
 {
-	int vid;
-	bool isValue;
+	int vid;//value or id of variable
+	bool isValue;//if true, vis is value - not id of variable
 }var;
 
-char * readWord(FILE *file, int maxLength)
+char * readWord(FILE *file)//returns NULL if it fails to read word
 {
 	char *str;
-	if((str = malloc(sizeof(char)*maxLength))==NULL)return NULL;
+	if((str = malloc(sizeof(char)*MAXLENGTH))==NULL)return NULL;
 	if(fscanf(file, "%s", str)==EOF)return NULL;
 	return str;
 }
 
-int wcl(FILE *file)
+int wcl(FILE *file)//counts lines just as wc -l
 {
 	char buff;
 	int lines=1;
@@ -36,11 +36,11 @@ int wcl(FILE *file)
 	fseek(file, 0, SEEK_SET);
 	return lines;
 }
-void readToTab(FILE *file, line tab[])
+void readToTab(FILE *file, line tab[])//reads whole file into tab of lines
 {
 	char *buff;
 	int i=0;
-	while((buff=readWord(file, MAXLENGTH))!=NULL)
+	while((buff=readWord(file))!=NULL)
 	{
 		switch(i%3)
 		{
@@ -58,7 +58,7 @@ void readToTab(FILE *file, line tab[])
 		i++;
 	}
 }
-bool compStr(char *str1, char *str2)
+bool compStr(char *str1, char *str2)//compares strings, true if strings are the same
 {
 	int i=0;
 	while(str1[i]!='\0' && str2[i]!='\0')
@@ -68,7 +68,7 @@ bool compStr(char *str1, char *str2)
 	}
 	return true;
 }
-void tagToId(line tag[], int tabf[], var tabv[], int length)
+void tagToId(line tag[], int tabf[], var tabv[], int length)//translates tags into ids
 {
 	for(int i=0; i<length; i++)
 	{
@@ -87,7 +87,7 @@ void tagToId(line tag[], int tabf[], var tabv[], int length)
 		}
 	}
 }
-void makeScript(char *fileName)
+void makeScript(char *fileName)//returns structure representing scrips of robot
 {
 	FILE *file = fopen(fileName, "r");
 	int lines = wcl(file);
@@ -97,6 +97,8 @@ void makeScript(char *fileName)
 	var *third = malloc(sizeof(var)*lines);
 
 	readToTab(file, tab);
+
+	tagToId(tab, first, third, lines);
 
 }
 int main()
